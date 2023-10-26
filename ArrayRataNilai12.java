03d78a3353616e2f666ff2d5c5e5a5327cfe926f
geriiy <gheriyaj@gmail.com>
2023-10-26 12:52:08 +0700

    modifikasi3

--- ArrayRataNilai12.java
+++ ArrayRataNilai12.java
@@ -5,21 +5,33 @@ public class ArrayRataNilai12 {
         Scanner input12 = new Scanner(System.in);
         
 
-        int[] nilaiMhs = new int[10];
-        double total = 0;
-        double rata2;
+       
+        int nilaiMhs=0, banyakMhs, lulus=0, tidakLulus=0;
+        double total1 = 0, total2 = 0;
+        double rata2, rata1;
+        System.out.print("Masukkan jumlah Mahasiswa     : ");
+        banyakMhs = input12.nextInt();
         
-        for (int i = 0; i < nilaiMhs.length; i++) {
+        for (int i = 0; i < banyakMhs; i++) {
             System.out.print("Masukkan nilai Mahasiswa ke-"+(i+1)+" : ");
-            nilaiMhs[i] = input12.nextInt();
-        }
+            nilaiMhs = input12.nextInt();
+            if (nilaiMhs > 70){
+                total1 += nilaiMhs;
+                lulus++;
+            }
+            else {
+                total2 += nilaiMhs++;
+                tidakLulus++;
 
-        for (int i = 0; i < nilaiMhs.length; i++) {
-            total += nilaiMhs[i];
+            }
+            
         }
+ 
 
-        rata2 = total/nilaiMhs.length;
-            System.out.println("Rata-rata nilai = "+rata2);
+        rata2 = total1/lulus;
+            System.out.println("Rata-rata nilai Lulus       = "+rata2);
+        rata1 = total2/tidakLulus;
+            System.out.println("Rata-rata nilai Tidak Lulus = "+rata1);
 input12.close();
     }
     
