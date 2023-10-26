d239f87152ecafb7b5c568e86efe1f56c6ccfcf5
geriiy <gheriyaj@gmail.com>
2023-10-26 14:34:07 +0700

    modifikasi4

--- LinearSearch12.java
+++ LinearSearch12.java
@@ -1,16 +1,37 @@
+import java.util.Scanner;
+
 public class LinearSearch12 {
     public static void main(String[] args) {
-        int[] arrayInt = {34, 18, 26, 48, 72, 20, 56, 63};
-        int key = 20;
-        int hasil = 0;
+        Scanner input12 = new Scanner(System.in);
 
+        int hasil=0;
+        System.out.print("Masukkan jumlah elemen array: ");
+        int n = input12.nextInt();
+        int[] arrayInt = new int[n];
+
+        for (int i = 0; i < n; i++) {
+            System.out.print("Masukkan elemen array ke-"+(i)+" : ");
+            arrayInt[i] = input12.nextInt();
+            
+        }
+        System.out.print("Masukkan key yang ingin anda cari: ");
+        int key = input12.nextInt();
         for (int i = 0; i < arrayInt.length; i++) {
             if (arrayInt[i] == key) {
                 hasil = i;
-                break;
+                System.out.println("Key ada di posisi indeks ke-"+hasil);
+              
             }
-        }
-        System.out.println("Key ada dalam array pada posisi indeks ke-"+hasil);
+
+            }
+            
+        
+      
+     
+
+        
+        input12.close();
     }
     
 }
+
