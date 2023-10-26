79beedca875574d904686e17ecc1a6835470d765
geriiy <gheriyaj@gmail.com>
2023-10-26 12:09:54 +0700

    Modifikasi2

--- ArrayNilai12.java
+++ ArrayNilai12.java
@@ -6,15 +6,20 @@ public class ArrayNilai12 {
 
         int [] nilaiAkhir = new int[10];
 
-        for (int i = 0; i < 10; i++) {
+        for (int i = 0; i < nilaiAkhir.length; i++) {
             System.out.print("Masukkan nilai akhir ke-"+i+" : ");
             nilaiAkhir[i] = input12.nextInt();
         }
-        for (int i = 0; i < 10; i++) {
-            System.out.println("Masukkan nilai akhir ke-"+i+" adalah "+nilaiAkhir[i]);
+        for (int i = 0; i < nilaiAkhir.length; i++) {
+            if (nilaiAkhir[i] > 70) {
+                System.out.println("Mahasiswa ke-"+i+" Lulus!");
         }
+            else {
+                System.out.println("Mahasiswa ke-"+i+" Tidak Lulus!");
+            }
         
 input12.close();
     }
     
+}
 }
